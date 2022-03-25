04a5de248ccc58b422fcd6c59e53a4b6e4307a77
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-24 23:10:31 -0300

    feat: add text script

---
+++ src/assets/text.js
@@ -0,0 +1,15 @@
+const about = `Olá, tudo bem? Meu nome é Lara Bertelli, tenho 19 anos e trabalho como modelo e Digital Influencer há 5 anos.
+Em 2017 iniciei um curso profissionalizante de modelo com duração de 3 meses e desde estão descobri minha paixão! Participei de inúmeros concursos de beleza e coleciono títulos importantes como 3ª lugar no Miss Teen Juiz de Fora 2019. 
+
+Atualmente conto com cerca de 150 parceiros anuais, trabalhando com as mais diversificadas divulgações. Com pouco mais de 7 mil seguidores, alcancei 1 milhão de impressões com meu trabalho no Instagram e mais de 4 milhões de visualização no Tik Tok. 
+Além de mostrar muitos trabalhos, também divido meu dia a dia conciliando faculdade, maternidade, casa, jobs, treinos, além de muitas dicas de beleza. 
+
+O meu público alvo além de serem jovens que adoram o mundo da moda, agora também conta com mamães na procura de acompanhar a maternidade e dicas para seus filhos. 
+
+No meu perfil irão encontrar destaques com alguns trabalhos realizados e feedbacks de parceiros e seguidores que me acompanham. 
+
+Será um prazer fazer parte da sua equipe de divulgação, acompanhem em seguida os meus serviços disponíveis!`;
+
+module.exports = {
+  about,
+};
