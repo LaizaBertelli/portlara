4f78cbbaf06f6baf6203f418bce0d8caf3a21612
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-24 21:36:25 -0300

    feat: implements header without nav-bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="h-10 p-1 bg-[#E5E5E5]">
+      <h1 className="text-lg text-[#F53B00] font-bold">@LARABERTELLI_</h1>
+    </div>
+  );
+}
