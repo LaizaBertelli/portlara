0c7d574faa2a660338c80b5914085b66e1aedd79
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-24 21:18:38 -0300

    feat: implements linktree with styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Linktree from './pages/Linktree';
+import Home from './pages/Home';
 
 function App() {
   return (
--- src/pages/Linktree.jsx
+++ src/pages/Linktree.jsx
@@ -7,11 +7,12 @@ const THENEWS_URL = 'https://thenewscc.com.br/indicacao/?grsf=zsfrqq';
 export default function Linktree () {
   const navigate = useNavigate();
   return (
-    <div>
-      <button onClick={ () => window.open(CONTACT_URL) }>CONTATO</button>
-      <button onClick={ () => navigate('/home') }>MIDIA KIT</button>
-      <button onClick={ () => window.open(GALMBOX_URL) }>GLAMBOX</button>
-      <button onCLick={ () => window.open(THENEWS_URL) }>THE NEWS</button>
+    <div className="bg-linktree h-screen w-screen flex-col flex justify-around items-center">
+      <div className="bg-profile w-52 h-52 rounded-full"></div>
+      <button className="bg-[#F53B00] text-white h-10 w-4/5 text-xl text-bold" onClick={ () => window.open(CONTACT_URL) }>CONTATO</button>
+      <button className="bg-[#F53B00] text-white h-10 w-4/5 text-xl text-bold" onClick={ () => navigate('/home') }>MIDIA KIT</button>
+      <button className="bg-[#F53B00] text-white h-10 w-4/5 text-xl text-bold" onClick={ () => window.open(GALMBOX_URL) }>GLAMBOX</button>
+      <button className="bg-[#F53B00] text-white h-10 w-4/5 text-xl text-bold" onCLick={ () => window.open(THENEWS_URL) }>THE NEWS</button>
     </div>
   );
 }
