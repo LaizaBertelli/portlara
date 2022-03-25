7dda64df7db87aba11847bba684d7c47aceb18bc
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-24 21:18:09 -0300

    feat: init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'profile': "url('../assets/placeholder.jpg')",
+        'linktree': "url('../assets/full-bloom.png')",
+      }
+    }
+  },
+  plugins: [],
+}
