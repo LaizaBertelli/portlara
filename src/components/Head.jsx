8293728c0c81cac6e14d1e5e0f41ae21e5e93508
LaizaBertelli <laizabertelli@hotmail.com>
2022-03-24 23:10:42 -0300

    feat: change head styles

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <div className="mt-10 w-full flex justify-around bg-white h-60">
       <section className="flex basis-2/4 flex-col justify-center p-4">
         <p className="text-sm font-thin">DIGITAL INFLUENCER E MODELO</p>
-        <h2 className="text-3xl">Melhore suas vendas com propagandas</h2>
+        <h2 className="mx-auto text-2xl font-semibold">Melhore suas vendas com propagandas</h2>
       </section>
       <section className="flex basis-2/4 flex-col h-full place-content-evenly">
         <div className="m-auto bg-profile w-28 h-28 rounded-full"></div>
