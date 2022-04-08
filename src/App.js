0c9c8fac9b278a97f8f5fb9be95fb0b1272c8d75
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-08 17:49:49 -0300

    feat: add budget route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Linktree from './pages/Linktree';
 import Home from './pages/Home';
+import Budget from './pages/Budget';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Linktree /> } />
         <Route path="/home" element={ <Home /> } />
+        <Route path="budget" element={ <Budget /> } /> 
       </Routes>
     </div>
   );
