1449e9d9ddceb7c9d2275747fa6b990e23e5388b
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-08 17:49:33 -0300

    feat: add function to go back to home through header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div className="h-10 w-screen p-1 bg-[#E5E5E5] fixed top-0">
-      <h1 className="text-lg text-[#F53B00] font-bold">@LARABERTELLI_</h1>
+      <button onClick={ () => navigate('/home') }>
+        <h1 className="text-lg text-[#F53B00] font-bold">@LARABERTELLI_</h1>
+      </button>
     </div>
   );
 }
