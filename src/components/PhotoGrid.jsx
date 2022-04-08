3c31a25c903e954f9ef9c4afc93f1c18ef26f4a3
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-08 17:24:12 -0300

    feat: add photoGrid section

---
+++ src/components/PhotoGrid.jsx
@@ -0,0 +1,15 @@
+export default function PhotoGrid() {
+  return(
+    <div className="grid grid-rows-3 grid-flow-col gap-1 m-4">
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+      <div className="bg-placeholder-img bg-cover  w-100 h-28"></div>
+    </div>
+  );
+}
