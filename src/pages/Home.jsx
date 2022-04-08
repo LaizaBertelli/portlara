51163c0f859f0f90d5ecc05d92bfa0ab5db9bf67
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-08 17:24:42 -0300

    feat: render new components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import Header from "../components/Header";
 import Head from "../components/Head";
 import Footer from '../components/Footer';
+import PhotoGrid from "../components/PhotoGrid";
 import { about } from "../assets/text.js";
 import { GrInstagram } from 'react-icons/gr';
 import { FaTiktok } from 'react-icons/fa';
+import WppButton from "../components/wppButton";
 
 export default function Home() {
   return (
     <div className="h-full bg-[#E5E5E5]">
+      <WppButton />
       <Header />
       <Head />
       {/* about section */}
@@ -18,6 +21,10 @@ export default function Home() {
           <GrInstagram color="#F53B00" size="50px" />
           <FaTiktok color="#F53B00" size="50px" />
         </section>
+      </section>
+      {/* previous jobs - carousel */}
+      <section>
+        { <PhotoGrid /> }
       </section>
       <Footer />
     </div>
