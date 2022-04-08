068b67ff7b061c31ff6e6c7d55b511cadccd54c4
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-08 17:50:25 -0300

    feat: implement function to navigate to Budget page

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Head() {
+  const navigate = useNavigate();
   return (
     <div className="mt-10 w-full flex justify-around bg-white h-60">
       <section className="flex basis-2/4 flex-col justify-center p-4">
@@ -7,7 +10,7 @@ export default function Head() {
       </section>
       <section className="flex basis-2/4 flex-col h-full place-content-evenly">
         <div className="m-auto bg-profile bg-cover w-28 h-28 rounded-full"></div>
-        <button className="bg-[#F53B00] text-white text-lg p-2 w-3/5 m-auto rounded-full">CONTRATE</button>
+        <button className="bg-[#F53B00] text-white text-lg p-2 w-3/5 m-auto rounded-full" onClick={ () => navigate('/budget') }>CONTRATE</button>
       </section>
     </div>
   );
