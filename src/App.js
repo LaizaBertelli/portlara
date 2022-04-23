64a003f4d4eaa76ed612a7c801c306c27068e68e
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-23 20:40:55 -0300

    feat: implements  job detailes routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Linktree from './pages/Linktree';
 import Home from './pages/Home';
 import Budget from './pages/Budget';
+import Photos from './pages/Photos';
+import Reels from './pages/Reels';
+import Divulgacoes from './pages/Divulgacoes';
+import Lives from './pages/Lives';
+import Desfiles from './pages/Desfiles';
 
 function App() {
   return (
@@ -10,7 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={ <Linktree /> } />
         <Route path="/home" element={ <Home /> } />
-        <Route path="budget" element={ <Budget /> } /> 
+        <Route path="/budget" element={ <Budget /> } />
+        <Route path="/photos" element={ <Photos /> } />
+        <Route path="/reels" element={ <Reels /> } />
+        <Route path="/divulgacoes" element={ <Divulgacoes /> } />
+        <Route path="/lives" element={ <Lives /> } />
+        <Route path="/desfiles" element={ <Desfiles /> } />
       </Routes>
     </div>
   );
