1ca9a28db690dc198ad27e0e948a5d9193fe52cb
LaizaBertelli <laizabertelli@hotmail.com>
2022-04-23 20:41:13 -0300

    feat: create job infos file

---
+++ src/assets/jobsInfos.js
@@ -0,0 +1,101 @@
+export const type1 = {
+  type: 'BICAS/GUARARÁ',
+  priceRange: '150 - 550',
+  oneTime: 'R$150 Uma vez',
+  biweekly: 'R$280 Quinzenal',
+  weekly: 'R$550 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível.',
+};
+
+export const type2 = {
+  type: 'JUIZ DE FORA',
+  priceRange: '250 - 950',
+  oneTime: 'R$250 Uma vez',
+  biweekly: 'R$480 Quinzenal',
+  weekly: 'R$950 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível.',
+};
+
+export const type3 = {
+  type: 'OUTRAS CIDADES',
+  priceRange: '200 - 750',
+  oneTime: 'R$200 Uma vez',
+  biweekly: 'R$380 Quinzenal',
+  weekly: 'R$750 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível. Para reservar datas de fotos em outras cidades é necessário o adiantamento de 50% do valor.',
+};
+
+export const type4 = {
+  type: 'STUDIO',
+  priceRange: '150 - 550',
+  oneTime: 'R$150 Uma Vez',
+  biweekly: 'R$280 Quinzenal',
+  weekly: 'R$550 Semanal',
+  extraInfo: 'O lojista entrega as peças que serão fotografadas, são disponibilizados 3 cenários diferentes no qual podem ser escolhidos um de preferência. Quando finalizadas o lojista será avisado podendo ir buscar.'
+};
+
+export const type5 = {
+  type: 'FOTÓGRAFO',
+  priceRange: '250 - 950',
+  oneTime: 'R$250 Uma vez',
+  biweekly: 'R$480 Quinzenal',
+  weekly: 'R$950 Semanal',
+  extraInfo: 'As fotos são realizadas pelo fotógrafo parceiro Denis Damasceno, garantimos a praticidade dos lojistas entregando fotos editadas, com cenários diferentes e conceituais. O projeto inclui modelo, local, fotógrafo e edição, com resultados profissionais e atrativos para clientes novos. O lojista também pode optar pela locação que preferir, gerando acréscimos em relação ao transporte.'
+};
+
+export const type6 = {
+  type: '1 VÍDEO',
+  priceRange: '60',
+  oneTime: '1 vídeo',
+  weekly: '2 looks',
+  biweekly: '+ fotos',
+  extraInfo: '*Taxa de looks extras: R$20,00',
+}
+
+export const type7 = {
+  type: 'BICAS/GUARARÁ',
+  priceRange: '150',
+  oneTime: '',
+  weekly: '',
+  biweekly: '',
+  extraInfo: 'Valores com duração máxima de 5 horas. para reservar a data de live é necessário o adiantamento de 50% do valor. taxa de hora extra: R$40,00',
+}
+
+export const type8 = {
+  type: 'OUTRAS CIDADES',
+  priceRange: '250',
+  oneTime: '',
+  weekly: '',
+  biweekly: '',
+  extraInfo: 'Valores com duração máxima de 5 horas. para reservar a data de live é necessário o adiantamento de 50% do valor. taxa de hora extra: R$40,00',
+}
+
+export const type9 = {
+  type: 'BICAS/GUARARÁ',
+  priceRange: '100 - 360',
+  oneTime: 'R$100 Uma vez',
+  biweekly: 'R$180 Quinzenal',
+  weekly: 'R$360 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível.',
+};
+
+export const type10 = {
+  type: 'OUTRAS CIDADES',
+  priceRange: '150 - 560',
+  oneTime: 'R$150 Uma vez',
+  biweekly: 'R$280 Quinzenal',
+  weekly: 'R$560 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível.',
+};
+
+export const type11 = {
+  type: 'ONLINE',
+  priceRange: '80 - 300',
+  oneTime: 'R$80 Uma vez',
+  biweekly: 'R$140 Quinzenal',
+  weekly: 'R$300 Semanal',
+  extraInfo: 'Me desloco até o local e realizamos as fotos no cenário disponível.',
+};
+
+
+
